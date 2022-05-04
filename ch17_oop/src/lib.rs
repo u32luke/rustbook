3104ca36e6ef6ff098073cb18365b9ef86190efd
u32luke <u32Luke@gmail.com>
2022-05-04 14:57:54 -0400

    17.3 Change code, initial commit for blog project

--- ch17_oop/src/lib.rs
+++ ch17_oop/src/lib.rs
@@ -1,31 +1,19 @@
-pub struct AveragedCollection {
-    list: Vec<i32>,
-    average: f64,
+pub struct Post {
+    state: Option<Box<dyn State>>,
+    content: String,
 }
 
-impl AveragedCollection {
-    pub fn add(&mut self, value: i32) {
-        self.list.push(value);
-        self.update_average();
-    }
-
-    pub fn remove(&mut self) -> Option<i32> {
-        let result = self.list.pop();
-        match result {
-            Some(value) => {
-                self.update_average();
-                Some(value)
-            }
-            None => None,
+impl Post {
+    pub fn new() -> Post {
+        Post {
+            state: Some(Box::new(Draft {})),
+            content: String::new(),
         }
     }
-
-    pub fn average(&self) -> f64 {
-        self.average
-    }
-
-    fn update_average(&mut self) {
-        let total: i32 = self.list.iter().sum();
-        self.average = total as f64 / self.list.len() as f64;
-    }
 }
+
+trait State {}
+
+struct Draft {}
+
+impl State for Draft {}
