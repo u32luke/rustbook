4900387887881c2784c11480bf391036ed176425
u32luke <u32Luke@gmail.com>
2022-05-05 07:40:44 -0400

    Add Ch13 iterators and closure

---
+++ ch13_closure/target/debug/deps/ch13_closure-4139faa92e45e5bf.d
@@ -0,0 +1,7 @@
+/home/lukepc/code/rustbook/ch13_closure/target/debug/deps/ch13_closure-4139faa92e45e5bf.rmeta: src/main.rs
+
+/home/lukepc/code/rustbook/ch13_closure/target/debug/deps/ch13_closure-4139faa92e45e5bf.d: src/main.rs
+
+src/main.rs:
+
+# env-dep:CLIPPY_ARGS=
---
+++ ch13_closure/target/debug/deps/ch13_closure-c29d6476c10b756d.d
@@ -0,0 +1,7 @@
+/home/lukepc/code/rustbook/ch13_closure/target/debug/deps/ch13_closure-c29d6476c10b756d.rmeta: src/main.rs
+
+/home/lukepc/code/rustbook/ch13_closure/target/debug/deps/ch13_closure-c29d6476c10b756d.d: src/main.rs
+
+src/main.rs:
+
+# env-dep:CLIPPY_ARGS=
