8b996d3b3426bbf1eb86eb55eebd3779290f9bce
Luke <u32luke@gmail.com>
2022-10-10 17:22:29 -0400

    Ch6.1 upload

---
+++ Ch6_enums/6.1/src/main.rs
@@ -0,0 +1,15 @@
+use std::fmt::Display;
+
+fn main() {
+  #[derive(Debug)]  
+  enum IpAddr {
+        V4(String),
+        V6(String),
+    }
+
+  let home = IpAddr::V4(String::from("127.0.0.1"));
+
+  let loopback = IpAddr::V6(String::from("::1"));
+
+  println!("{:?}", loopback);
+}
